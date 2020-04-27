b70441a390fc29ad3ed46fde092baf91efd1d154
martinkarlik <47499520+martinkarlik@users.noreply.github.com>
2020-04-27 09:22:27 +0200

    playback just works

--- Source/creator.py
+++ Source/creator.py
@@ -11,49 +11,53 @@ from Source import audio_io
 
 
 interface = interface.CreatorInterface()
-recording = audio_io.RecordingThread(1)
-playback = audio_io.PlaybackThread(2)
+recording = None
+playback = None
 
 while interface.running:
     interface.update()
 
     if interface.audio_manager.recording_state["started"]:
+        recording = audio_io.RecordingThread(1)
         recording.start()
 
     if interface.audio_manager.recording_state["in_process"]:
         pass
 
     elif interface.audio_manager.recording_state["stopped"]:
-        recording.keep_on = False
-        recording.join()
+        recording.stop()
 
     if interface.audio_manager.playback_state["started"]:
-        playback.mic_data = recording.get_data()
+        playback = audio_io.PlaybackThread(2)
+        playback.set_data(recording.get_data())
         playback.start()
+    elif interface.audio_manager.playback_state["in_process"]:
 
-    if playback.done:
-        interface.audio_manager.playback_state["stopped"] = True
-        playback.done = False
-        print(interface.audio_controller.full_audio_data)
+        # playback.get_chunk()
+        # playback.process_chunk()
+        # playback.play_chunk()
 
+        if playback.done:
+            interface.audio_manager.playback_state["stopped"] = True
+            playback.done = False
 
-    # audio_data = interface.audio_controller.full_audio_data
-    #
-    # audio_data = np.array(audio_data)
-    #
-    # total_frames = np.sum(audio_data[:, 1])
-    # audio_data[:, 1] = np.divide(audio_data[:, 1], total_frames)
-    #
-    # for sample in audio_data:
-    #     sample[1] = int(sample[1] * len(rec_data))
-    #
-    # audio_data[len(audio_data) - 1][1] += abs(len(rec_data) - np.sum(audio_data[:, 1]))
-    #
-    # positional_data, reverb_data = audio_processing.split_audio_data(audio_data)
-    #
-    # output = audio_processing.apply_binaural_filtering(rec_data, positional_data)
-    # sd.play(output)
-    # sd.wait()
+            audio_data = interface.audio_controller.full_audio_data
+            print(audio_data)
+            audio_data = np.array(audio_data)
+
+            total_frames = np.sum(audio_data[:, 1])
+            audio_data[:, 1] = np.divide(audio_data[:, 1], total_frames)
+
+            for sample in audio_data:
+                sample[1] = int(sample[1] * len(recording.get_data()))
+
+            audio_data[len(audio_data) - 1][1] += abs(len(recording.get_data()) - np.sum(audio_data[:, 1]))
+
+            positional_data, reverb_data = audio_processing.split_audio_data(audio_data)
+
+            output = audio_processing.apply_binaural_filtering(recording.get_data(), positional_data)
+            sd.play(output)
+            sd.wait()
 
 
 
