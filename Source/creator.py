76bc1bd587eb9b2e7103f603cfe6f70b742278c4
VictorBuch <47211196+VictorBuch@users.noreply.github.com>
2020-04-27 14:27:22 +0200

    Loading and saving csv files

--- Source/creator.py
+++ Source/creator.py
@@ -1,6 +1,9 @@
 import sofa
 import numpy as np
 import sounddevice as sd
+import pandas as pd
+import csv
+import os.path
 import soundfile as sf
 from scipy.signal import *
 import threading
@@ -9,10 +12,11 @@ from Source import interface
 from Source import audio_processing
 from Source import audio_io
 
-
 interface = interface.CreatorInterface()
 recording = None
 playback = None
+
+number_of_recordings_done = 0
 
 while interface.running:
     interface.update()
@@ -42,8 +46,10 @@ while interface.running:
             playback.done = False
 
             audio_data = interface.audio_controller.full_audio_data
-            print(audio_data)
+            # print(audio_data)
             audio_data = np.array(audio_data)
+            # print(audio_data)
+            # print(audio_data.shape)
 
             total_frames = np.sum(audio_data[:, 1])
             audio_data[:, 1] = np.divide(audio_data[:, 1], total_frames)
@@ -55,6 +61,16 @@ while interface.running:
 
             positional_data, reverb_data = audio_processing.split_audio_data(audio_data)
 
+            # ---------------------------------------- HANDLE CSV FILE -------------------------------------------------
+
+            csv_file_name = "../Dependencies/csv_data/positional_data" + str(number_of_recordings_done) + ".csv"
+            if not os.path.isfile(csv_file_name):
+                pd.DataFrame(positional_data).to_csv(csv_file_name, header=None, index=None)
+                number_of_recordings_done += 1
+            csv_loaded = list(csv.reader(open(csv_file_name)))
+            csv_loaded = np.array(csv_loaded)
+            print("csv shape: ", csv_loaded.shape)
+
             output = audio_processing.apply_binaural_filtering(recording.get_data(), positional_data)
             sd.play(output)
             sd.wait()
@@ -62,11 +78,9 @@ while interface.running:
         # sd.play(output)
         # sd.wait()
 
-
         # tmp_reverb_signal = audio_processing.add_reverb(rec_data, recording.sampling_freq, interface.audio_controller.current_audio_data)
     # if interface.audio_manager.playback_started:
     #     recording.play()
-
 
 # TODO Listener Interface
 # TODO Continues recording of flexible length (stopped when told to do so)
