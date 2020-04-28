dac7478e668e1281c3778569fc9bb5b05477d444
VictorBuch <47211196+VictorBuch@users.noreply.github.com>
2020-04-28 13:37:56 +0200

    Nothing :-1:

--- Source/audio_io.py
+++ Source/audio_io.py
@@ -32,7 +32,8 @@ class RecordingThread(AudioIOThread):
         super().__init__()
         self.rec_data = np.array([[]])
 
-        self.rec_stream = sd.InputStream(samplerate=self.sampling_freq, channels=2, blocksize=self.chunk_samples, callback=self.callback)
+        self.rec_stream = sd.InputStream(samplerate=self.sampling_freq, channels=2, blocksize=self.chunk_samples,
+                                         callback=self.callback)
 
     def callback(self, indata, frames, time, status):
 
@@ -73,11 +74,11 @@ class PlaybackThread(AudioIOThread):
         self.filter_state_left = None
         self.filter_state_right = None
 
-        self.play_stream = sd.OutputStream(samplerate=self.sampling_freq, channels=2, blocksize=self.chunk_samples, callback=self.callback)
+        self.play_stream = sd.OutputStream(samplerate=self.sampling_freq, channels=2, blocksize=self.chunk_samples,
+                                           callback=self.callback)
 
     def run(self):
         self.play_stream.start()
-
 
     def callback(self, outdata, frames, time, status):
 
@@ -86,13 +87,14 @@ class PlaybackThread(AudioIOThread):
         start_index = self.counter * frames
         end_index = (self.counter + 1) * frames
 
-        ir_ear_right = self.hrtf_database[0.8].Data.IR.get_values(indices={"M": (self.counter * 4) % 360, "R": 0, "E": 0})
-        ir_ear_left = self.hrtf_database[0.8].Data.IR.get_values(indices={"M": (self.counter * 4) % 360, "R": 1, "E": 0})
+        ir_ear_right = self.hrtf_database[0.8].Data.IR.get_values(
+            indices={"M": (self.counter * 4) % 360, "R": 0, "E": 0})
+        ir_ear_left = self.hrtf_database[0.8].Data.IR.get_values(
+            indices={"M": (self.counter * 4) % 360, "R": 1, "E": 0})
 
         if self.filter_state_left is None and self.filter_state_right is None:
             self.filter_state_left = np.zeros([len(ir_ear_left) - 1])
             self.filter_state_right = np.zeros([len(ir_ear_left) - 1])
-
 
         outdata[:, 0], self.filter_state_left = \
             lfilter(ir_ear_left, 1, play_data_transposed[0, start_index:end_index], zi=self.filter_state_left)
